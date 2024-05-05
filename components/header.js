494b835fd96a5df9a609a6bc10ca4d8ddcd6067b
Flavio Arzamendia <flavioarzamendia2019@gmail.com>
2024-05-05 14:34:55 -0300

    cambio accesibilidad

--- components/header.js
+++ components/header.js
@@ -47,13 +47,15 @@ const Header = () => {
             <button
               onClick={() => setNavbarOpen(true)}
               className="flex items-center px-3 py-2  text-slate-200 hover:text-white "
+              alt="Open menu" 
             >
-              <Bars3Icon className="h-12 w-12 " title="Open menu" alt="Open menu" />
+              <Bars3Icon className="h-12 w-12 " title="Open menu" />
             </button>
           ) : (
             <button
               onClick={() => setNavbarOpen(false)}
               className="flex items-center px-3 py-2  text-slate-200 hover:text-white hover:border-white text-xl font-semibold"
+              alt="Open menu" 
             >
               <XMarkIcon className="h-12 w-12" title="close menu" alt="close menu"/>
             </button>
