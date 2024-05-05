65a4cd7a32f41bad22dbee36ed531f5196fdc7db
Flavio Arzamendia <flavioarzamendia2019@gmail.com>
2024-05-05 14:43:45 -0300

    prueba

--- components/header.js
+++ components/header.js
@@ -47,7 +47,8 @@ const Header = () => {
             <button
               onClick={() => setNavbarOpen(true)}
               className="flex items-center px-3 py-2  text-slate-200 hover:text-white "
-              title="Open menu" 
+              title="Open menu"
+              id="Abrir menu"
             >
               <Bars3Icon className="h-12 w-12 " title="Open menu" />
             </button>
