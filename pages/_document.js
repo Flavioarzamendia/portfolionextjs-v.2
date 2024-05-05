af518cffb4726f1e2b4bd221c99765236b03cd05
Flavio Arzamendia <flavioarzamendia2019@gmail.com>
2024-05-05 01:28:04 -0300

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -20,6 +20,13 @@ export default function Document() {
           rel="stylesheet"
         />
 
+        <link rel="apple-touch-icon" sizes="180x180" href="/img/assets/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/img/assets/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/img/assets/favicon-16x16.png"/>
+        <link rel="manifest" href="/img/assets/site.webmanifest"/>
+        <link rel="mask-icon" href="/img/assets/safari-pinned-tab.svg" color="#5bbad5"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="theme-color" content="#ffffff"/>
 
       </Head>
       <body >
