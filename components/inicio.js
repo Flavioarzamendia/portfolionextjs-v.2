f73841fe15162371b3e6d23ca0621fb00f083133
Flavio Arzamendia <flavioarzamendia2019@gmail.com>
2024-05-05 13:56:39 -0300

    preload

--- components/inicio.js
+++ components/inicio.js
@@ -150,6 +150,7 @@ const Inicio = () => {
             <div className="bg-gray-900 rounded-md shadow-lg overflow-hidden text-white">
               <Image
                 loading='lazy'
+                rel="preload"
                 alt="SEO Optimizacion"
                 className="w-full object-cover"
                 height="720"
@@ -181,6 +182,7 @@ const Inicio = () => {
             <div className="bg-gray-900 rounded-md shadow-lg overflow-hidden text-white">
               <Image
                 loading='lazy'
+                rel="preload"
                 alt="Actualizacion y mantenimiento"
                 className="w-full object-cover"
                 height="720"
@@ -221,6 +223,7 @@ const Inicio = () => {
             <div className="bg-white rounded-md shadow-lg overflow-hidden md:col-span-2 lg:col-span-1">
               <Image
                 loading='lazy'
+                rel="preload"
                 alt="Podcas fm proyecto"
                 className="w-full  object-cover"
                 height="720"
@@ -251,6 +254,7 @@ const Inicio = () => {
             <div className="bg-white rounded-md shadow-lg overflow-hidden">
               <Image
                 loading='lazy'
+                rel="preload"
                 alt="App delivey proyecto"
                 className="w-full  object-cover"
                 height="720"
@@ -281,6 +285,7 @@ const Inicio = () => {
             <div className="bg-white  rounded-md shadow-lg overflow-hidden">
               <Image
                 loading='lazy'
+                rel="preload"
                 alt="Airbnb web estilo proyecto"
                 className="w-full object-cover"
                 height="720"
