892514a34d9a123e98265d7bbba063a2c41647a5
Flavio Arzamendia <flavioarzamendia2019@gmail.com>
2024-05-07 13:47:25 -0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com'
-      }
-    ],
-  }
-  
-}
 
-module.exports = nextConfig
+  const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+  })
+  module.exports = withBundleAnalyzer({})
+
