c81218de63d56836ca0ce71f8e289f9468840185
Flavio Arzamendia <flavioarzamendia2019@gmail.com>
2024-05-08 20:30:47 -0300

    actualizar

--- components/ofrece.js
+++ components/ofrece.js
@@ -148,7 +148,7 @@ const   Ofrece = () => {
             ">
               <div className="p-6 bg-blue-600 text-white text-center">
                 <h3 className="text-4xl font-bold mb-4">Plan Básico</h3>
-                <p className="text-6xl font-bold mb-4">$99</p>
+                <p className="text-6xl font-bold mb-4">$125</p>
                 <p className="mb-6">Desde USD</p>
               </div>
               <div className="p-6 px-10">
@@ -230,7 +230,7 @@ const   Ofrece = () => {
             transition ease-in-out delay-150 lg:hover:-translate-y-1 lg:hover:scale-110 lg:duration-300 ">
               <div className="p-6 bg-blue-600 text-white text-center">
                 <h3 className="text-4xl font-bold mb-4">Plan Estándar</h3>
-                <p className="text-6xl font-bold mb-4">$199</p>
+                <p className="text-6xl font-bold mb-4">$250</p>
                 <p className="mb-6">Desde USD</p>
               </div>
               <div className="p-6 px-10">
